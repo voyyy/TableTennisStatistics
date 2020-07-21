7cfe1837ba7f807a4da440c69fed40a6c8072614
voyyy <voytec411@gmail.com>
2020-07-21 19:21:52 +0200

    Match History is now sorted by date

--- lib/UI/screens/MatchHistory.dart
+++ lib/UI/screens/MatchHistory.dart
@@ -56,78 +56,76 @@ class _MatchHistoryState extends State<MatchHistory> {
 
   Widget build(BuildContext context) {
     final matchProvider = Provider.of<CRUDModel>(context);
-    return Column(
-      children: <Widget>[
-        AppBar(
-          title: const Text('Historia meczów'),
-          backgroundColor: Styles.primaryColor,
-        ),
-        StreamBuilder(
-          stream: matchProvider.fetchMatchesAsStream(),
-          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasData) {
-              if (snapshot.data.documents.length == 0)
-                return Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 250, 0, 0),
-                  child: Center(
-                    child: Text('Brak meczów w historii'),
-                  ),
-                );
-              else {
-                matches = snapshot.data.documents
-                    .map((doc) => Match.fromMap(doc.data, doc.documentID))
-                    .toList();
-                return ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: matches.length,
-                  itemBuilder: (context, int index) {
-                    return Dismissible(
-                      key: Key(matches[index].id),
-                      confirmDismiss: (direction) => confirmDelete(
-                          direction, matches[index].id, matchProvider),
-                      direction: DismissDirection.endToStart,
-                      background: Align(
-                        alignment: Alignment(0.9, 0),
-                        child: Icon(
-                          Icons.delete_forever,
-                          size: 60,
-                        ),
-                      ),
-                      child: MatchItem(
-                        color: matches[index].result['voy'] >
-                                matches[index].result['dmn']
-                            ? Styles.voyColor
-                            : Styles.dmnColor,
-                        date: _dateFormat.format(
-                          matches[index].date,
-                        ),
-                        matchResult: matches[index].result['voy'].toString() +
-                            ':' +
-                            matches[index].result['dmn'].toString(),
-                        onTap: () => Navigator.pushNamed(
-                            context, '/matchDetails',
-                            arguments: {'data': matches[index]}),
-                      ),
-                    );
-                  },
-                );
-              }
-            } else
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Historia meczów'),
+        backgroundColor: Styles.primaryColor,
+      ),
+      body: StreamBuilder(
+        stream: matchProvider.fetchMatchesAsStream(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+            if (snapshot.data.documents.length == 0)
               return Padding(
                 padding: const EdgeInsets.fromLTRB(0, 250, 0, 0),
-                child: Container(
-                  height: 70,
-                  width: 70,
-                  child: CircularProgressIndicator(
-                    strokeWidth: 12,
-                    valueColor:
-                        AlwaysStoppedAnimation<Color>(Styles.primaryColor),
-                  ),
+                child: Center(
+                  child: Text('Brak meczów w historii'),
                 ),
               );
-          },
-        ),
-      ],
+            else {
+              matches = snapshot.data.documents
+                  .map((doc) => Match.fromMap(doc.data, doc.documentID))
+                  .toList();
+              matches.sort((a, b) => b.date.compareTo(a.date));
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: matches.length,
+                itemBuilder: (context, int index) {
+                  return Dismissible(
+                    key: Key(matches[index].id),
+                    confirmDismiss: (direction) => confirmDelete(
+                        direction, matches[index].id, matchProvider),
+                    direction: DismissDirection.endToStart,
+                    background: Align(
+                      alignment: Alignment(0.9, 0),
+                      child: Icon(
+                        Icons.delete_forever,
+                        size: 60,
+                      ),
+                    ),
+                    child: MatchItem(
+                      color: matches[index].result['voy'] >
+                              matches[index].result['dmn']
+                          ? Styles.voyColor
+                          : Styles.dmnColor,
+                      date: _dateFormat.format(
+                        matches[index].date,
+                      ),
+                      matchResult: matches[index].result['voy'].toString() +
+                          ':' +
+                          matches[index].result['dmn'].toString(),
+                      onTap: () => Navigator.pushNamed(context, '/matchDetails',
+                          arguments: {'data': matches[index]}),
+                    ),
+                  );
+                },
+              );
+            }
+          } else
+            return Padding(
+              padding: const EdgeInsets.fromLTRB(0, 250, 0, 0),
+              child: Container(
+                height: 70,
+                width: 70,
+                child: CircularProgressIndicator(
+                  strokeWidth: 12,
+                  valueColor:
+                      AlwaysStoppedAnimation<Color>(Styles.primaryColor),
+                ),
+              ),
+            );
+        },
+      ),
     );
   }
 }
